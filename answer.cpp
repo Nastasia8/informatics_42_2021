3d6bd0a1d5e96c43e325b6453149286c66e3075a
Ivan Khamraev <crgvidonlol@gmail.com>
2021-10-10 20:53:59 +0300

    bugs fixed again : )

--- answer.cpp
+++ answer.cpp
@@ -5,7 +5,7 @@ int main()
 {
     float x = 0;
     long double answer;
-    for ( x = 0.11; x < 0.37; x += 0.05 )
+    for ( x = 0.11; x <= 0.36; x += 0.05 )
     {
         answer = ((pow(sin(x), 3) + pow(cos(x), 3)) * log(x));
         std::cout << answer << std::endl;
