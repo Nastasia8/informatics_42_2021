bec358335f8174b9085f5afc5e136c9e2753804d
Sasha1602 <91780565+Sasha1602@users.noreply.github.com>
2021-10-17 15:59:19 +0300

    task 2.2

--- les.cpp
+++ les.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 #include <cmath>
+#include <locale.h>
 
 using namespace std;
 
-int square (int height, int width)
+int square (int height, int width)                                                // площадь квадрата
 {
     return height * width;
 }
-int square (int radius, float Pi)
+int square (int radius, float Pi)                                                 // площадь круга
 {
     return Pi * radius;
 }
-int square (int first_foun, int second_foun, int height)
+int square (int first_foun, int second_foun, int height)                          // площадь трапеции
 {
     return first_foun * second_foun * height * 0.5;
 }
 
-int perimitre ( int width)
+int perimitre (int width)                                                         // периметр квадрата
 {
     return 4 * width;
 }
-int perimitre (int radius, float Pi)
+int perimitre (int radius, float Pi)                                              // периметр круга
 {
     return 2 * Pi * radius;
 }
-int perimitre (int first_foun, int second_foun, int side_one, int side_two)
+int perimitre (int first_foun, int second_foun, int side_one, int side_two)       // периметр трапеции
 {
     return first_foun + second_foun + side_two + side_one;
 }
 
 int main()
 {
+    setlocale (LC_ALL,".866");
+
     const float Pi = 3.14;
+
     int height, width, radius, first_foun, second_foun, side_one, side_two;
+
+    cout << "Введите height, width, radius, first_foun, second_foun, side_one, side_two" << endl;
+
     cin >> height;
     cin >> width;
     cin >> radius;
@@ -41,12 +48,12 @@ int main()
     cin >> side_one;
     cin >> side_two;
 
-    cout << "square = " << square (height, width) <<endl;
-    cout << "square = " << square (radius, Pi) <<endl;
-    cout << "square = " << square (first_foun, second_foun, height) <<endl;
-    cout << "perimitre = " << perimitre (width) <<endl;
-    cout << "perimitre = " << perimitre (radius, Pi) <<endl;
-    cout << "perimitre = " << perimitre (first_foun, second_foun, side_one, side_two) <<endl;
+    cout << "square = " << square (height, width) << endl;
+    cout << "square = " << square (radius, Pi) << endl;
+    cout << "square = " << square (first_foun, second_foun, height) << endl;
+    cout << "perimitre = " << perimitre (width) << endl;
+    cout << "perimitre = " << perimitre (radius, Pi) << endl;
+    cout << "perimitre = " << perimitre (first_foun, second_foun, side_one, side_two) << endl;
 
-return 0;
+    return 0;
 }
