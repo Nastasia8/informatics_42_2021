71c3adb9553905ddb449ab529c0d846787e13377
Kir0Ka <92202455+Kir0Ka@users.noreply.github.com>
2021-10-21 15:39:14 +0300

    Create AppA.cpp

---
+++ firstmathupgrade/AppA.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include "objects.h"
+#include "function.h"
+
+using namespace std;
+
+
+
+int main(){
+    float y,xbegin,xend,xtr;
+
+    xbegin = 1.2;
+    xend = 2.2;
+    xtr = 0.2;
+    
+
+    cout<<"First task"<<endl;
+    
+    function(a,b,y,xbegin,xend,xtr);
+
+    
+    
+
+}
