31581a83cd022d45e6e37c58ef963edfcfdc4f07
Katerina92z1 <e.katerinka.z@mail.ru>
2021-10-22 17:18:58 +0300

    Изменили

---
+++ inf/AppA.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+#include "objects.h"
+#include "function.h"
+
+using namespace std;
+
+
+
+int main() {
+    float y, x, xk, dx;
+
+    x = 1.2;
+    xk = 2.2;
+    dx = 0.2;
+
+
+    cout << "First task" << endl;
+
+    function(a, b, y, x, xk, dx);
+
+
+
+
+}
