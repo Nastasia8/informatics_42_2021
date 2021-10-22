288c6dac3ab65a1c40470f56ff23ddfaea99e6f5
Пользователь <dantedmc.5@mail.ru>
2021-10-22 18:53:06 +0300

    task3

---
+++ test3/AppB.cpp
@@ -0,0 +1,12 @@
+#include "function.h"
+#include <iostream>
+#include <math.h>
+#include "objects.h"
+using namespace std;
+
+ int main(){
+
+     float mas_x[] = {1.16, 1.32, 1.47, 1.65, 1.93};
+     function_1 (a, mas_x, int(sizeof(mas_x)/sizeof (float)));
+ 
+ }
