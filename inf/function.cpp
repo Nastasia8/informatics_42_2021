2c8bb2b9bd97c19a42be6f882906f6e0a43e2766
Katerina92z1 <92202637+Katerina92z1@users.noreply.github.com>
2021-10-22 22:15:04 +0300

    Update function.cpp

--- inf/function.cpp
+++ inf/function.cpp
@@ -10,7 +10,7 @@ void function(const float a, const float b, float y, float x, float xk, float dx
         cout << "y = " << y << endl;
     }
 }
-void function(const float a, const float b, float xarr[], int size, float y) {
+void function(const float a, const float b, float xs[5], int size, float y) {
     for (auto x : xs) {
         y = ((pow(a, x) - pow(b, x)) / log10(a / b)) * pow(a * b, 1 / 3);
         cout << "y = " << y << endl;
