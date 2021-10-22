e413966e4bdc07e4eff1fcfa95d70c5a676d7ed4
Violettam88 <violettam88@yandex.ru>
2021-10-22 22:06:00 +0300

    Создали

---
+++ violetta_mishina/AppB.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+#include "objects.h"
+#include "function.h"
+
+using namespace std;
+
+
+
+int main() {
+    float y;
+
+    cout << "Second task" << endl;
+
+    float xs[5] = { 1.9, 2.15, 2.34, 2.73, 3.16 };
+
+    function(a, b, xs, (int)(sizeof(xs) / sizeof(xs[0])), y);
+
+
+
+}
+
+
---
+++ violetta_mishina/function.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void function(const float a, const float b, float y, float x, float xk, float dx) {
+    for (; x < xk; x += dx)
+    {
+        y = (a * sqrt(x) - b * log(a)/log(5)) / log10(abs(x - 1));
+        cout << "y = " << y << endl;
+    }
+}
+void function(const float a, const float b, float xs[5], int size, float y) {
+    for (auto x : xs) {
+        y = (a * sqrt(x) - b * log(a)/log(5)) / log10(abs(x - 1));
+        cout << "y = " << y << endl;
+    }
+
+}
