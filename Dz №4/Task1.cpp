4b226117db87733e7e3c0a1149a47fb5cb5a14eb
Sasha1602 <91780565+Sasha1602@users.noreply.github.com>
2021-10-23 11:42:28 +0300

    Laba 4 task 1

---
+++ Dz №4/Task1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float x;
+    float y;
+    float c = 0;                        // c это итог выбранного действия
+    int operation_number;
+
+    cout << "select the operation number from 1 to 5. Where 1 is addition, 2 is multiplication, 3 is division,4 is exponentiation, 5 is subtraction" <<endl;
+    cin >> operation_number;
+    cout << "enter variables x" <<endl;
+    cin >> x;                            // x  это первая переменная
+    cout << "enter variables y" <<endl;
+    cin >> y;                            //  y это вторая переменная
+
+    switch (operation_number)
+    {
+    case 1:
+        c = x + y;
+        cout << c <<endl;
+        break;
+
+    case 2:
+        c = x * y;
+        cout << c <<endl;
+        break;
+
+    case 3:
+        c = x / y;
+        cout << c <<endl;
+        break;
+
+    case 4:
+        c = pow(x, y);
+        cout << c <<endl;
+        break;
+
+    case 5:
+        c = x - y;
+        cout << c <<endl;
+        break;
+    }
+return 0;
+}
