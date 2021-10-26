5be58a03bd628e36042fedf41e59a2058d4da53d
Katerina92z1 <e.katerinka.z@mail.ru>
2021-10-26 19:42:57 +0300

    с

---
+++ zadanie/5.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+void k(int x, int y, int z) {
+	float f1, f2;
+	float D = y * y - 4 * x * z;
+	if (D > 0) {
+		f1 = (-y + sqrt(D)) / 2 * x;
+		f2 = (-y - sqrt(D)) / 2 * x;
+		cout << "Rez" << f1 << " " << f2;
+	}
+	else if (D == 0) {
+		f1 = -y / 2 * x;
+		cout << "Rez" << f1;
+	}
+	else
+	{
+		cout << "Net";
+	}
+
+
+}
+
+
+int main()
+{
+	int x, y, z;
+	cin >> x >> y >> z;
+	k(x, y, z);
+
+	return 0;
+
+
+}
