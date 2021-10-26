a11f34d646d36998f620ece5652e6aaaa615dd0d
Violettam88 <violettam88@yandex.ru>
2021-10-26 19:53:57 +0300

    Я пыталась.. Номер 8 и 2 не знаю

---
+++ lb3/homework6.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int funksion(int n) 
+{
+    int k;
+    int m = 1;
+    while (n != 0)
+    {
+        k = n % 10;
+        n = n / 10;
+        if (s != 0)
+        {
+            m *= k;
+        }
+    }
+    return m;
+}
+
+int main() {
+    int n;
+    cout << "Введите шестизначное число: ";
+    cin >> n;
+    if (n < 100000 || n > 999999) 
+    {
+        cout << "Произведение цифр числа N = " << funksion(n) << endl;
+    }
+    else
+    {
+        cout << "Вы ввели не шестизначное число" << endl;
+    }
+    return 0;
+}
---
+++ lb3/homework8.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int const ROW = 3;
+	int const COL = 4;
+	int mas[ROW][COL];
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < COL; j++)
+		{
+			mas[i][j] = rand() % 100;
+		}
+	}
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < COL; j++)
+		{
+			cout<< mas[i][j]<<"\t";
+		}
+		cout << endl;
+	}
+	return 0;
+}
