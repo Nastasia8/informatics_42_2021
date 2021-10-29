b827bcc80cf5f6afb034f4d6c8fc815803fcb2a4
ksyukornilich <ksyu_kornilich@mail.ru>
2021-10-29 12:51:32 +0300

    Задание 4 (2)

---
+++ laba 1/lol1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{  
+   int i=0;
+   float A[i]={0.08,0.26,0.35,0.41,0.53};
+   int a=2, b=3;
+
+   float y=0;
+   float g=0;
+   
+   float Xn=0.11;
+   float Xk=0.36;
+   float dX=0.05;
+
+   int count; 
+   cout<<"write 1 or 2";
+   cin >> count;
+   
+
+   switch (count)
+
+   {
+    case 1:
+    {
+   while (i<=4)
+   {  
+      y=asin(pow(A[i],a))+acos(pow(A[i],b));    // задача B
+      cout<<"y = "<< y << endl;
+      i++;
+   }
+   break;
+    }
+   case 2:
+  {
+   while (Xn<=Xk)
+   {
+      g=asin(pow(Xn,a))+acos(pow(Xn,b));    // задача A
+      cout<<"g = "<< g << endl;
+      Xn=Xn+dX;
+   }
+   break;
+   }
+return 0;
+
+} }
