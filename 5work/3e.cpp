25a2e02cfd974b573e539561a09cae2e46df4d46
Katerina92z1 <e.katerinka.z@mail.ru>
2021-11-04 10:10:52 +0300

    Все, кроме 5

---
+++ 5work/3e.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+
+int main()
+{
+	srand(time(NULL));
+
+	int n;
+	int min = 888;
+
+	cin >> n;
+	int *a = new int[n];
+
+for (int i = 0; i < n; i++) {
+
+	a[i] = rand() % 100;
+}
+
+for (int i = 0; i < n; i++)
+{
+	cout << a[i] << "\t";
+}
+for (int i = 0; i < n; i++) {
+	if (a[i] < min) {
+		min = a[i];
+	}
+}
+	cout << endl;
+	cout << min;
+
+	return 0;
+}
+
+
