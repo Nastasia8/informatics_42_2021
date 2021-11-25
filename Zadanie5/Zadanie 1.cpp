5747a40a50ff44a5d20d629c7cc6d9f97027aa71
ivan231412 <katrovsev@mail.ru>
2021-11-25 21:07:33 +0300

    при поддержке Александра Бутусова

---
+++ Zadanie5/Zadanie 1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+void zs(int* a, int* d, int* f)
+{
+	cout << *a << *d << *f;
+}
+
+int main()
+{
+	int q = 2;
+	int w = 3;
+	int e = 4;
+	zs(&q, &w, &e);
+
+}
---
+++ Zadanie5/Zadanie 3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+
+    int minimal = 1000;
+    int rasmer;
+
+    cout << "Enter rasmer = " << endl;
+    cin >> rasmer;
+
+    cout << endl;
+
+
+    int* massiv = new int[rasmer];
+
+
+    for (int r = 0; r < rasmer; r++)
+    {
+        massiv[r] = rand() % 100;
+
+    }
+
+    for (int r = 0; r < rasmer; r++)
+    {
+        cout << massiv[r] << "\t";
+
+
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int r = 0; r < rasmer; r++)
+    {
+        if (massiv[r] < minimal)
+        {
+            minimal = massiv[r];
+        }
+    }
+    cout << "Minimum element of array = " << minimal << endl;
+
+    delete[] massiv;
+}
