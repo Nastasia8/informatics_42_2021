2632b24085e821b5eb81ab15bfd0a410a504efc8
Katerina92z1 <e.katerinka.z@mail.ru>
2021-11-26 19:55:57 +0300

    ...

---
+++ work6/4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y; //вводятся числа изначально имевшие общ делитель, иначе ничего не будет 
+    int a = x;
+    int c = y;
+   if (x > y)
+   {
+      x = x % y;
+   }
+   else {
+      y = y % x;
+      
+    } 
+
+    while ((x != 0) && (y != 0));
+    cout << a / (x + y) * c;
+}
