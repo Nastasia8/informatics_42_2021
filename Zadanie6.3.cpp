dfd61c917acd472c89ba06c338b305cd6fde0728
ivan231412 <92202798+ivan231412@users.noreply.github.com>
2021-11-26 22:16:55 +0300

    Add files via upload

---
+++ Zadanie6.3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+
+    int x, y;
+    cout << "Enter x and y : ";
+    cin >> x >> y;
+    do
+    {
+        if (x > y)
+        {
+            x = x % y;
+        }
+         
+        else {
+            y = y % x;
+        }
+
+    } while ((x != 0) && (y != 0));
+    cout << "Nod = " << (x + y);
+
+
+
+
+}
