1a4fb38240056b9ca8167ea4d25fa30f099bc855
algrimbut <alexander.butusov.03@mail.ru>
2021-11-29 11:00:36 +0300

    task 7

---
+++ 7 task/1.1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int rows,columns;
+    int index = 1;
+    cout << "Enter rows = ";
+    cin >> rows;
+    cout << "Enter columns = ";
+    cin >> columns;
+    int ** matrix = new int*[rows];
+    for (int i = 0; i <rows; i++)
+    {
+        matrix[i] = new int[columns];
+    }
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i][j] = index;
+            index++;
+        }
+
+    } 
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cout << matrix[i][j] << "\t";
+        }
+        
+        cout << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = columns-1; j >= 0; j--)
+        {
+            cout << matrix[i][j] << "\t";
+        }
+        
+        cout << endl;
+    }   
+
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] matrix[i];
+    }
+}
---
+++ 7 task/4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+long double factorial(int k) {
+    if(k < 0) 
+        return 0; 
+    if (k == 0) 
+        return 1; 
+    else 
+        return k * factorial(k - 1); 
+}
+
+int main () {
+    int n, sum = 0;
+    cout << "Enter numbers = ";
+    cin >> n;
+    for(int k = 1; k <= n; k++) {
+        sum += -1 * k * ((5 - k) / factorial(k));
+    }
+
+    cout << sum;
+
+}
