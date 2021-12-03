8de14c5b8957e64976591daa1f272307c69cf3f5
Sasha1602 <91780565+Sasha1602@users.noreply.github.com>
2021-12-04 00:19:03 +0300

    Лаба 6 задание 2

---
+++ DZ_№6/2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void calc(int num,int base)
+{
+
+    if (num == 0)
+        return;
+    int rem = num /*введённое число пользователем*/ % base/*ссистема счисления*/;
+    if (rem <= 9)
+        {
+            calc(num/base,base);
+            cout << rem;
+        }
+    else
+        {
+            calc(num/base,base);
+            cout << char(rem - 10 + 'A');
+        }
+}
+
+int main() {
+
+    int num;
+    int base;
+    cout << "Enter a number from 0 to 350" << endl;
+    cin >>num;
+    cout << "Enter a number systems" <<endl;
+    cin >>base;
+    if ((num <= 350) && (num >= 0))
+    {
+        calc(num,base);
+        cout <<endl;
+    } else
+        {
+            cout << "they said that you need to enter from 0 to 350" << endl;
+        }
+    system("pause");
+    return 0;
+}
