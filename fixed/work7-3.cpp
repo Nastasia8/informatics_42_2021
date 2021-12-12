6df6e3c5120feec352adb0d31f32a974bfd73057
Katerina92z1 <e.katerinka.z@mail.ru>
2021-12-12 14:41:55 +0300

    Исправила

---
+++ fixed/work7-3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+    int number;
+
+    cin >> number;
+
+    while (number > 1) 
+    {
+        if (number % 2 == 0) //проверяем на четность
+        {
+            number /= 2; 
+        }
+        else
+        {
+            number *= 3; //умножаем на 3
+            number += 1; //увеличиваем на 1
+            number /= 2; // делим на 2
+        }
+        cout << number << endl;
+    } //добавила
+}
---
+++ fixed/work7-4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+ float f(int k) //можно, вроде писать люой тип данный, тк числа без запятых
+{ 
+    if (k < 0)
+    {
+        return 0;
+    }
+    if (k == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return k * f(k - 1);
+    }
+}
+
+int main()
+{
+    int n, sum = 0;
+    cin >> n;
+    for (int k = 1; k <= n; k++)
+    {
+        sum += -1 * k * ((5-k) / f(k));
+    }
+    cout << sum;
+}
