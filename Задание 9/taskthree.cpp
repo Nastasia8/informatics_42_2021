96abf01563d5d13c454059c886e3a33460b44c0c
SergeyLeMoNChiK <batanertyp7388@gmail.com>
2021-12-20 15:17:18 +0300

    Added comments

--- Задание 9/taskthree.cpp
+++ Задание 9/taskthree.cpp
@@ -8,60 +8,77 @@ class Massive
 {
 
  public:
-    int rows;
-    int cols;
-    int **element;
-    int **twoelement;
-    int **Dopelement;
+    int rows; // определяем переменную для строк
+    int cols; // определяем переменную для столбов 
+    int **element; // определяем двумерный динамический массив 
+    int **twoelement; // двумерный динамический массив для транспоранции матрицы
+    int **Dopelement; // двумерный динамический массив для вычисления суммы двух матриц
+
     //Конструктор двумерного динамического массива
-    Massive(int a, int b)
+    Massive(int a, int b) // Конструктор двумерного динамического массива
     {
-        rows = a;
-        cols = b;
-        element = new int* [rows];
-        for (int i = 0; i <rows; i++)
+        rows = a; // объявление переменной для rows
+        cols = b; // объявление переменной для cols
+        element = new int* [rows]; // определяем двумерный массив
+        for (int i = 0; i <rows; i++) 
+
         {
             element[i] = new int[cols];
         }
+
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < cols; j++)
+
             {
-                element[i][j] = rand() % 201 + (-100);
+                element[i][j] = rand() % 201 + (-100); // заполнение двумерного массива рандомными числами
             }
         }
     }
     //Деструктор 
-    ~Massive()
+    ~Massive() // Деструктор
     {
-        for (int i = 0; i < rows; i++)
+        for (int i = 0; i < rows; i++) // освобождаем ресурсы двумерного динамического массива
         {
             delete[] element[i];
         }
-        delete element;
-        for (int i = 0; i < cols; i++)
+
+        delete element; // освобождаем массив
+
+        for (int i = 0; i < cols; i++) // освобождаем ресурсы двумерного динамического массива для транспонирования матрицы
+
         {
-            delete[] twoelement[i];
+
+            delete[] twoelement[i]; 
+
         }
-        delete twoelement;
-        for (int i = 0; i < rows; i++)
+
+        delete twoelement; // освобождаем массив
+
+        for (int i = 0; i < rows; i++) // освобождаем ресурсы двумерного динамического массива для вычисления суммы двух матриц
+
         {
+
             delete[] Dopelement[i];
+
         }
-        delete Dopelement;
+
+        delete Dopelement; // освобождаем массив
         
     }
-    //Метод отображения двумерного массива 
-    void Info()
+
+    //Метод отображения двумерного массива
+
+    void Info() // объявлем функцию для отображения информации о двумерном динамическом массиве
 
     {
-        for (int i = 0; i < rows; i++)
+        for (int i = 0; i < rows; i++) // цикл для строк
 
         {
 
-            for (int j = 0; j < cols; j++)
+            for (int j = 0; j < cols; j++) // цикл для столбцов
 
-                cout << element[i][j] << " ";
+                cout << element[i][j] << " "; // отображаем матрицу
 
             cout << endl;
               
@@ -71,13 +88,14 @@ class Massive
     }
 
     //Транспонация матрицы
-    void transport()
+
+    void transport() // объявлем функцию для отображения информации о транспоранции матрицы
 
     {
 
-        twoelement = new int * [cols];
+        twoelement = new int * [cols]; // объявляем двумерный динамический массив в котором храним перевернутую матрицу
 
-        for (int i = 0; i < cols; i++)
+        for (int i = 0; i < cols; i++) 
 
         {
 
@@ -85,16 +103,16 @@ class Massive
 
         }
 
-        for (int i = 0; i < rows; i++)
+        for (int i = 0; i < rows; i++) // цикл для строк
 
         {
 
-            for (int j = 0; j < cols; j++)
+            for (int j = 0; j < cols; j++) // цикл для столбов
 
             {
 
-                twoelement[i][j] = element[j][i];
-                cout <<  twoelement[i][j] << " "; 
+                twoelement[i][j] = element[j][i]; // транспонируем матрицу
+                cout <<  twoelement[i][j] << " "; // выводим 
 
             } 
 
@@ -106,11 +124,12 @@ class Massive
     }
 
  //Сумма матриц
-    void Matricasumm()
+
+    void Matricasumm() // объявлем функцию для отображения информации о сумме 2-х матриц
 
     {
 
-        Dopelement = new int * [rows];
+        Dopelement = new int * [rows]; // объявляем двумерный динамический массив для суммы 2-х матриц
 
         for (int i = 0; i < rows; i++)
 
@@ -119,17 +138,17 @@ class Massive
             Dopelement[i] = new int[cols];
         }
 
-        for (int i = 0; i < rows; i++)
+        for (int i = 0; i < rows; i++) // цикл для строк
 
         {
 
-            for (int j = 0; j < cols; j++)
+            for (int j = 0; j < cols; j++) // цикл для столбов
 
             {
 
-                Dopelement[i][j] = rand() % 201 + (-100);
+                Dopelement[i][j] = rand() % 201 + (-100); // заполнение массива
 
-                cout << Dopelement[i][j] << " ";
+                cout << Dopelement[i][j] << " "; // вывод массива
 
             }
 
@@ -139,16 +158,16 @@ class Massive
 
         cout << endl;
 
-        for (int i = 0; i < rows; i++)
+        for (int i = 0; i < rows; i++) // цикл для строк
 
         {
 
-            for (int j = 0; j < cols; j++)
+            for (int j = 0; j < cols; j++) // цикл для столбов
 
             {
 
-                Dopelement[i][j] = Dopelement[i][j] + element[i][j];
-                cout << Dopelement[i][j] << " ";
+                Dopelement[i][j] = Dopelement[i][j] + element[i][j]; // складываем 2 матрицы
+                cout << Dopelement[i][j] << " "; // выводим
 
             }
 
@@ -159,23 +178,24 @@ class Massive
     }
 
     //изменение массива
-    void Change()
+
+    void Change() // объявлем функцию для замены отрицательных значений в матрице
 
     {
 
-        for (int i = 0; i < rows; i++)
+        for (int i = 0; i < rows; i++) // цикл для строк
 
         {
 
-            for (int j = 0; j < cols; j++)
+            for (int j = 0; j < cols; j++) // цикл для столбов
 
             {
 
-                if (element[i][j] < 0)
+                if (element[i][j] < 0) // условие для определения отрицательного числа 
 
                 {
 
-                    element[i][j] = 0;
+                    element[i][j] = 0; // отрицательные числа заменяются на 0
 
                 }
 
@@ -191,34 +211,34 @@ class Massive
 int main() 
 
  {
-    srand(time(NULL));
+    srand(time(NULL)); // функция для рандомных чисел
 
-    Massive First(3,3);
+    Massive First(3,3); // размер 1 массива
 
-    Massive Second (4, 4);
+    Massive Second (4, 4); // размер 2 массива
 
-    Massive Third (2, 2);
+    Massive Third (2, 2); // размер 3 массива
 
-    First.Info();
+    First.Info(); // Применение функций Info на объект класса Massive
 
-    Second.Info();
+    Second.Info(); // Применение функций Info на объект класса Massive
 
-    Second.Change();
+    Second.Change(); // Применение функций Change на объект класса Massive
 
-    Second.Info();
+    Second.Info(); // Просмотр изменений сматрицы
 
-    First.transport();
+    First.transport(); // Применение функций Transport на объект класса Massive
 
-    Third.Info();
+    Third.Info(); //  Просмотр изменений матрицы
 
-    Third.Matricasumm();
+    Third.Matricasumm(); // Применение функций Matricasumm на объект класса Massive
 
 
     //Сумма нечетных элементов
 
-    cout << "The sum of odd elements in each row of a two-dimensional array = { ";
+    cout << "The sum of odd elements in each row of a two-dimensional array = { "; // вывод 
 
-    int *sum1 = new int[First.rows];
+    int *sum1 = new int[First.rows]; // объявление одномерного динамичекого массива для поиска суммы нечётных элементов в строке
 
     for (int i = 0; i < First.rows; i++)
 
@@ -229,18 +249,18 @@ int main()
         for (int j = 0; j < First.cols; j++)
         {
 
-            if(t % 2 != 0)
+            if(t % 2 != 0) // ищем нечётные числа
 
             {
 
-                sum += First.element[i][j];
+                sum += First.element[i][j]; // считаем сумму
 
             }
 
-            t++;
+            t++; // переходим к следующему числу
         }
 
-        sum1[i] = sum;
+        sum1[i] = sum; // присваеваем значение суммы  сумме нечётных элементов в строке 
 
     }
 
@@ -248,7 +268,7 @@ int main()
 
     {
 
-        cout << sum1[i] << " ";
+        cout << sum1[i] << " "; // выводим
 
     }
 
@@ -258,29 +278,29 @@ int main()
 
     cout << "The maximum array element in each column among odd values = { ";
 
-    int *maxcolsnech = new int[First.cols];
+    int *maxcolsnech = new int[First.cols]; // объявление одномерного динамичекого массива для поиска максимального элемента массива в каждом столбце среди нечетных значений
 
     for (int i = 0; i < First.cols; i++)
 
     {
 
-        int max = -1000;
+        int max = -1000; // объявляем переменую максимального числа 
 
         for (int j = 0; j < First.rows; j++)
 
         {
 
-            if((First.element[j][i] % 2 != 0) && First.element[j][i] > max)
+            if((First.element[j][i] % 2 != 0) && First.element[j][i] > max) // поиск нужного элемента
 
             {
 
-                max = First.element[j][i];
+                max = First.element[j][i]; // присваеваем значение элемента максимальному
 
             }
 
         }
 
-        maxcolsnech[i] = max;
+        maxcolsnech[i] = max; // присваеваем максимальный элемент максимальному нечётному в столбе
 
     }
 
@@ -288,17 +308,17 @@ int main()
 
     {
 
-        if (maxcolsnech[i] != -1000)
+        if (maxcolsnech[i] != -1000) // Проверка на наличие нечётных
 
         {
 
-            cout << maxcolsnech[i] << " ";
+            cout << maxcolsnech[i] << " "; //выводим
 
         } else 
 
         {
 
-            cout << "Nothing! ";
+            cout << "Nothing! "; // Если нет нечетных чисел
 
         }
 
@@ -310,23 +330,23 @@ int main()
 
     cout << "The average value in each column = {  ";
 
-    float *AverageStolb = new float[First.cols];
+    float *AverageStolb = new float[First.cols]; //объявление одномерного динамичекого массива для поиска среднего значения в каждом столбце
 
     for (int i = 0; i < First.cols; i++)
 
     {
 
-        int Average = 0;
+        int Average = 0; // объявляем переменную среднего значения 
 
         for (int j = 0; j < First.rows; j++)
 
         {
 
-            Average += First.element[j][i];
+            Average += First.element[j][i]; // складываем
 
         }
 
-        AverageStolb[i] = Average/First.cols;
+        AverageStolb[i] = Average/First.cols; // ищем среднее значение в столбе
 
     }
 
@@ -334,14 +354,14 @@ int main()
 
     {
 
-        cout << AverageStolb[i] << " ";
+        cout << AverageStolb[i] << " "; // выводим
 
     }
 
     cout << "}" << endl;
-
+    // освобождаем переменные 363-366
     delete AverageStolb;
     delete sum1;
     delete maxcolsnech;
-    return 0;
+    return 0; // завершение
   }
