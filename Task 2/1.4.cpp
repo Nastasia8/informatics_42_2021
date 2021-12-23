27b0257bd3a601887127df403ceb138aea711f7b
DreadMaks <92202592+DreadMaks@users.noreply.github.com>
2021-12-23 16:30:59 +0300

    Add files via upload

---
+++ Task 2/1.4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void Vector(int x,int a,int y,int b,int z,int c)
+{
+    cout<<" Произведение векторов = "<<y*c - z*b<<"; "<<-1*(x*c - z*a)<<"; "<<x*b - y*a<< "; " << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int x = 1;
+    int y = 5;
+    int z = 3;
+    int a;
+    int b;
+    int c;
+ 
+    cout <<"Векторное произведение векторов"<<endl;
+    cout << "Введите переменные ";
+    cin >> a >> b >> c;
+    Vector(x, y, z, a, b, c);
+
+    
+    return 0;
+}
