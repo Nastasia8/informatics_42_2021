ed25927f9c0f283e4653cd292b68bc4265703331
DreadMaks <92202592+DreadMaks@users.noreply.github.com>
2021-12-23 05:36:16 +0300

    Add files via upload

---
+++ Task 2/2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+float Vector(int x, int y, int z)
+{
+    return sqrt(x * x + y * y + z * z);
+}
+void Vector(int x, int y, int z, float length)
+{
+    cout << "unit length vector = " << x / length << " " << y / length << " " << z / length << endl;
+
+}
+int main()
+{
+    int x = 1;
+    int y = 5;
+    int z = 3;
+
+    float length = Vector(x, y, z);
+
+    Vector(x, y, z, length);
+
+    return 0;
+}
