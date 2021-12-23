2f7d8f9b56068df587746bb7f423986aeef0787e
DreadMaks <92202592+DreadMaks@users.noreply.github.com>
2021-12-23 17:38:27 +0300

    Add files via upload

---
+++ Task 2/2.3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+
+    int side, area, perimeter;
+
+    cout << "Сторона квадрата: ";
+    cin >> side;
+ 
+
+    area = side * side;
+    perimeter = (side + side) * 2;
+
+    cout << "Площадь квадрата: " << area << endl;
+    cout << "Периметр квадрата: " << perimeter << endl;
+
+    return 0;
+}
