111c88fa2aefc86762014865ad890d4c01cbc873
DreadMaks <92202592+DreadMaks@users.noreply.github.com>
2021-12-23 16:11:24 +0300

    Add files via upload

---
+++ Task 2/1.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float ScalarProduct(int x,int a,int y,int b,int z,int c)
+{
+    return x*a + y*b + z*c;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int x = 1;
+    int y = 5;
+    int z = 3;
+    int a,b,c;
+
+    cout <<"1.3"<<endl;
+
+    cout <<"Введите переменные ";
+    cin >>a>>b>>c;
+    
+    cout <<"Скалярное произведение = "<<ScalarProduct(x, a, y, b, z, c)<<endl;
+ 
+    return 0;
+}
