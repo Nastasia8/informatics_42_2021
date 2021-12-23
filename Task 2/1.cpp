bf5ebaee7a1b78409ea2c5ad6c406b3e2dcaf47a
DreadMaks <92202592+DreadMaks@users.noreply.github.com>
2021-12-23 05:34:34 +0300

    Add files via upload

---
+++ Task 2/1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+
+int main()
+{
+    int x = 1;
+    int y = 5;
+    int z = 3;
+
+
+    float length = Vector(x, y, z);
+
+    cout << "vector length = " << length << endl;
+
+
+    return 0;
+}
