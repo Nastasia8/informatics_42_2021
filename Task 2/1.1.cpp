7d3e929cd8eaff5405a4ea980e375945de4e5e70
DreadMaks <92202592+DreadMaks@users.noreply.github.com>
2021-12-23 05:46:50 +0300

    Update 1.1.cpp

--- Task 2/1.1.cpp
+++ Task 2/1.1.cpp
@@ -2,15 +2,22 @@
 #include <cmath>
 using namespace std;
 
+float Vector(int x, int y, int z)
+{
+    return sqrt(x * x + y * y + z * z);
+}
+
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+
     int x = 1;
     int y = 5;
     int z = 3;
 
     float length = Vector(x, y, z);
 
-    cout << " length vector = " << length << endl;
-    
+    cout << " Длина вектора = " << length << endl;
+
     return 0;
 }
