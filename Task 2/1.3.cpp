12f704c32925d7a4bb8c195e9b46d2fe65dc57ad
DreadMaks <92202592+DreadMaks@users.noreply.github.com>
2021-12-23 16:16:21 +0300

    Add files via upload

---
+++ Task 2/1.3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float Scalar(int x,int a,int y,int b,int z,int c)
+{
+    return x*a + y*b + z*c;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int x = 1;
+    int y = 5;
+    int z = 3;
+    int a;
+    int b;
+    int c;
+
+    cout <<"Вычислите скалярное произведение векторов "<<endl;
+
+    cout <<"Введите переменные ";
+    cin >>a>>b>>c;
+    
+    cout <<"Скалярное произведение = "<< Scalar(x, a, y, b, z, c)<<endl;
+ 
+    return 0;
+}
