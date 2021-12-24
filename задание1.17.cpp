934329751667f22003033a2209584a555ad27c25
Maryrau2201 <93242768+Maryrau2201@users.noreply.github.com>
2021-12-25 02:13:56 +0300

    Update задание1.17.cpp

--- задание1.17.cpp
+++ задание1.17.cpp
@@ -12,7 +12,7 @@ int main(int args, char* argv[]) {
     float dx = 0.25;
 
     for (; x < xk; x += dx) {
-        cout << (a * sqrt(x) - b * log(a)/log(5)) / log10(abs(x - 1)) << endl;
+        cout << ((a+(tg(bx)*tg(bx)))/(b+(ctg(ax)*ctg(ax))))) << endl;
     };
 
     float xs[5] = { 1.9, 2.15, 2.34, 2.73, 3.16 };
