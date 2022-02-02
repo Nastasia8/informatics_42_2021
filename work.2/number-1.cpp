5b844f382f8ffa04dfad4484ec9190f5177f08e0
PollyFFF <fedupolina@yandex.ru>
2022-02-02 23:27:44 +0300

    сделано

---
+++ work.2/number-1.cpp
@@ -0,0 +1,17 @@
+﻿#include < iostream>
+#include <cmath>
+
+using namespace std; //пространство имён
+
+int main() //обязательная функция main
+{
+	int x = 1, y = 5, z = 3; // int представляет целое число
+	int i, n, k; // координаты вектора  b
+	cin >> i >> n >> k; // вводятся переменные i,n и k 
+	cout << "dlina" << (sqrt(x * x + y * y + z * z)) << endl; // выводится строка "...", а потом данные 
+	cout << "vektor" << (i / x + n / y + k / z) << endl; // endl текст выводится раздельно, на нескольких строках
+	cout << "ckal proizvedenie" << (x * i + y * n + z * k) << endl; 
+	cout << "vek proizvedenie" << ((y * k - n * z) - (x * k - z * i) + (x * n - y * i)) << endl;
+
+	return 0;
+} // конец
