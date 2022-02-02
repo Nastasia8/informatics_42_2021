d092b16c2132c9ffa9d22b86bf0bca9d00ac9494
PollyFFF <fedupolina@yandex.ru>
2022-02-02 23:29:45 +0300

    сделано

---
+++ work.3/AppB.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <cmath>
+#include "objects.h"
+#include "function.h"
+
+using namespace std; // пространство имен
+
+
+int main() 
+{
+    float y; // с плав.точкой
+
+    cout << "Second task" << endl; // вывод
+
+    float xs[5] = { 4.48, 3.56, 2.78, 5.28, 3.21 }; // массив
+
+    function(a, b, xs, (int)(sizeof(xs) / sizeof(xs[0])), y);
+
+}
+
