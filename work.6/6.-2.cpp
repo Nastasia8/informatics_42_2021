cb9ef0c93c7d697c57006a924bd7129217012767
PollyFFF <fedupolina@yandex.ru>
2022-02-03 21:32:49 +0300

    сделано

---
+++ work.6/6.-2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std; // пространство имён
+
+void translation(int x) // void не возвращает значения
+{
+    int binar = 0; // тип int
+    int octal = 0;
+    int hex = 0;
+    int v = x, s = x;
+
+    for (int i = 0; x > 0; i++) // в начале i = 0 и пока x > 0, к i прибавляется 1 и выполняется пустой оператор 
+    {
+        binar += (x % 2) * pow(10.0, i);
+        x /= 2;
+    }
+
+    for (int i = 0; v > 0; i++)
+    {
+        octal += (v % 8) * pow(10.0, i);
+        v /= 8;
+    }
+
+    for (int i = 0; s > 0; i++) // в начале i = 0 и пока s > 0, к i прибавляется 1 и выполняется пустой оператор 
+    {
+        hex += (s % 16) * pow(10.0, i);
+        s /= 16;
+    }
+    cout << binar << endl;
+    cout << octal << endl; // вывод
+    cout << hex << endl;
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+    while (x < 0 || x > 325) // цикл
+    {
+        cin >> x; 
+    }
+    translation(x);
+}
---
+++ work.6/6.-5 (a).cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std; // пространство имён
+
+int main()
+{
+    int a, b, c; // тип int
+    int n; // a + b
+    cin >> a >> b >> c; // ввод чисел, изначально имеющих общий делитель, в противном случае ничего не выводится 
+
+    if (a > b) // если a > b, то 
+    {
+        a = a % b;
+    }
+
+    else // иначе
+    {
+        b = b % a;
+    }
+
+    while ((a != 0) && (b != 0)); // пока выполн.условие
+
+    n = a + b;
+
+    if (n > c) 
+    {
+        n = a % c;
+    }
+
+    else
+    {
+        c = c % n;
+    }
+    while ((n != 0) && (c != 0));
+    cout << " NOD = " << (n + c); // вывод
+
+} 
